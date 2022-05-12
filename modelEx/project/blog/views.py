743f8ee767cb919c28a8c5db876c9895eb83844a
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-05-12 19:13:43 +0900

    feat: form 과제 완료

--- modelEx/project/blog/views.py
+++ modelEx/project/blog/views.py
@@ -20,45 +20,46 @@ def detail(request, id):
 
 def new(request):
     if request.method == 'POST':
-        form = BlogForm(request.POST)
+        form = BlogForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
-            return redirect('home.html')
+            save = form.save()
+            return redirect('detail', save.pk)
+
     else:
         form = BlogForm()
         return render(request, "new.html", {'form':form})
 
-def create(request):
-    new_blog = Blog()  # 빈 Blog 객체 생성
-    new_blog.title =  request.POST['title']
-    # POST요청으로 받은 'title'의 데이터를 위에서 만든 객체의 title 컬럼에 할당
-    new_blog.content = request.POST['content']
-    # POST요청으로 받은 'contents'의 데이터를 위에서 만든 객체의 contents 컬럼에 할당
-    new_blog.image = request.FILES['image']
-    new_blog.save() #실제 데이터베이스에 저장을 시키게 한다.
-    return redirect('detail',new_blog.id) #새로 만들어준 상세페이지로 redirect한다
+# def create(request):
+#     new_blog = Blog()  # 빈 Blog 객체 생성
+#     # new_blog.title = request.POST['title']
+#     # # POST요청으로 받은 'title'의 데이터를 위에서 만든 객체의 title 컬럼에 할당
+#     # new_blog.content = request.POST['content']
+#     # # POST요청으로 받은 'contents'의 데이터를 위에서 만든 객체의 contents 컬럼에 할당
+#     # new_blog.image = request.FILES['image']
+#     # new_blog.save() #실제 데이터베이스에 저장을 시키게 한다.
+#     return redirect('detail',new_blog.id) #새로 만들어준 상세페이지로 redirect한다
 
 def edit(request,id):
+
     edit_blog = get_object_or_404(Blog, pk = id) # 파라미터 id를 받아서 디비에 해당 객체를 가져온다.
+    if request.method == 'POST':
+        form = BlogForm(request.POST, request.FILES, instance=edit_blog)
+        if form.is_valid():
+            form.save()
+            return redirect('detail', edit_blog.pk)
+    else:
+        form = BlogForm(instance=edit_blog)
 
-    # if request.method == 'POST':
-    #     form = BlogForm(request.POST)
-    #     if form.is_valid():
-    #         form.save()
-    #         return redirect('home.html')
-    # else:
-    #     form = BlogForm()
-    #     return render(request, "edit.html", {'form':edit_blog})
-    return render(request, 'edit.html', {'blog':edit_blog})
+        return render(request, "edit.html", {'form':form})
 
-def update(request,id):
-    update_blog = get_object_or_404(Blog, pk=id) # 파라미터로 받은 id의 Blog 객체 가져옴
-    update_blog.title = request.POST['title']
-    update_blog.content = request.POST['content']
-    if request.FILES['image']:
-        update_blog.image = request.FILES['image']
-    update_blog.save()
-    return redirect('detail', update_blog.id)
+# def update(request,id):
+#     update_blog = get_object_or_404(Blog, pk=id) # 파라미터로 받은 id의 Blog 객체 가져옴
+#     update_blog.title = request.POST['title']
+#     update_blog.content = request.POST['content']
+#     if request.FILES['image']:
+#         update_blog.image = request.FILES['image']
+#     update_blog.save()
+#     return redirect('detail', update_blog.id)
 
 def delete(request,id):
     delete_blog  = get_object_or_404(Blog, pk = id) #파라미터로 받은 id에 해당하는 Blog객체 가져옴
